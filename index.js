783d2574e6b76d98383b8cc3cfe4be6712d182dd
Samuel Olabode <Solabode499@gmail.com>
2025-08-12 21:49:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ const MongoStore = require('connect-mongo')
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/developerBookmarks';
 
 // process.env.DB_URL
-mongoose.connect(dbUrl)
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "Connection error"));
